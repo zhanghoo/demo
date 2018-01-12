20fdba53909f6a6e1cf516d8a25f2c28d11c0917
Hoo <zhanghoo@163.com>
2018-01-12 10:44:39 +0800

    Update main.js

--- h5/aodi/main.js
+++ h5/aodi/main.js
@@ -57,20 +57,21 @@
         function setLoadingDisplay () {
             // Loading splash scene
             var splash = document.getElementById('splash');
+            var loadingText = splash.querySelector('.loading-text');
             var progressBar = splash.querySelector('.progress-bar span');
             cc.loader.onProgress = function (completedCount, totalCount, item) {
                 var percent = 100 * completedCount / totalCount;
                 if (progressBar) {
-                    //progressBar.style.width = percent.toFixed(2) + '%';
-                    progressBar.innerHTML = parseInt(percent);
+                    progressBar.style.width = percent.toFixed(2) + '%';
+                    loadingText.innerHTML = parseInt(percent) + '%';
                 }
             };
             splash.style.display = 'block';
-            //progressBar.style.width = '0%';
-            progressBar.innerHTML = '0';
+            progressBar.style.width = '0%';
+            loadingText.innerHTML = '0%';
 
             cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
-                splash.style.display = 'none';
+                // splash.style.display = 'none';
             });
         }
 
